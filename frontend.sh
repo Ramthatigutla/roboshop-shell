42e53fb5b64089d7fc9ddad9ae13e8f29acf6cbb
ram <ramthatigutla@gmail.com>
2023-05-27 00:15:18 +0530

    roboshop frontend check

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,15 @@
+echo -e "\e[31m installing nginx\e[0m"
 yum install nginx -y
-
+echo -e "\e[32m removing existing nginx webframe \e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[33m downlaoding new nginx content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[34m creating a directory to unzip downloaded content \e[0m"
 cd /usr/share/nginx/html
+echo -e "\e[35m dunzipping new downloaded nginx content \e[0m"
 unzip /tmp/frontend.zip
 #we need to copy config file
-
+echo -e "\e[36m enabling nginx \e[0m"
 systemctl enable nginx
+echo -e "\e[33m restarting nginx we server \e[0m"
 systemctl restart nginx
