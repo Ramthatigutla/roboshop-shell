ccc5f1837d2d5e94addf43639ee8112cf2df8aa1
ram <ramthatigutla@gmail.com>
2023-05-27 00:23:32 +0530

    roboshop frontend check

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,22 @@
 echo -e "\e[31m installing nginx\e[0m"
-yum install nginx -y
+yum install nginx -y >/tmp/roboshop.log
+
 echo -e "\e[32m removing existing nginx webframe \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+
 echo -e "\e[33m downlaoding new nginx content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+
 echo -e "\e[34m creating a directory to unzip downloaded content \e[0m"
 cd /usr/share/nginx/html
+
 echo -e "\e[35m unzipping new downloaded nginx content \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >/tmp/roboshop.log
+
 #we need to copy config file
+
 echo -e "\e[36m enabling nginx \e[0m"
-systemctl enable nginx
+systemctl enable nginx >/tmp/roboshop.log
+
 echo -e "\e[33m restarting nginx server \e[0m"
-systemctl restart nginx
+systemctl restart nginx >/tmp/roboshop.log
