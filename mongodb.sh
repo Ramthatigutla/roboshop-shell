736f3ca403281a1a5c925c17a1d893b24a501d8b
ram <ramthatigutla@gmail.com>
2023-05-27 00:49:08 +0530

    roboshop frontend check

--- mongodb.sh
+++ mongodb.sh
@@ -1,13 +1,13 @@
 echo -e "\e[31m copying mongodb repo file \e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo >>/tmp/roboshop.log
 
 echo -e "\e[31m installing mongodb \e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y >>/tmp/roboshop.log
 
 #modify the config file for listner address
 
 echo -e "\e[31m enabling mongodb \e[0m"
-systemctl enable mongod
+systemctl enable mongod >>/tmp/roboshop.log
 
 echo -e "\e[31m irestarting mongodb \e[0m"
-systemctl restart mongod
+systemctl restart mongod >>/tmp/roboshop.log
