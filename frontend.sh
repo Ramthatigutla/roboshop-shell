f22ebd70508f88ab7c042e7267891648b48579b6
ram <ramthatigutla@gmail.com>
2023-05-27 00:30:56 +0530

    roboshop frontend check

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo -e "\e[32m removing existing nginx webframe \e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[33m downlaoding new nginx content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log  2>/tmp/error.log
 
 echo -e "\e[34m creating a directory to unzip downloaded content \e[0m"
 cd /usr/share/nginx/html
