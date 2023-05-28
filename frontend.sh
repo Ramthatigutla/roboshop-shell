5a6bb7a466616c76de378ebe259897ccaa0be4c3
ram <ramthatigutla@gmail.com>
2023-05-28 19:27:22 +0530

    roboshop frontend check

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 echo -e "\e[31m installing nginx\e[0m"
-yum install nginx -y >>/tmp/roboshop.log
+yum install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[32m removing existing nginx webframe \e[0m"
-rm -rf /usr/share/nginx/html/* >>/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33m downlaoding new nginx content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/roboshop.log  2>/tmp/error.log
@@ -11,12 +11,13 @@ echo -e "\e[34m creating a directory to unzip downloaded content \e[0m"
 cd /usr/share/nginx/html
 
 echo -e "\e[35m unzipping new downloaded nginx content \e[0m"
-unzip /tmp/frontend.zip >>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-#we need to copy config file
+echo -e "\e[35m updating frontend configuration \e[0m"
+cp /home/centos/reboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[36m enabling nginx \e[0m"
-systemctl enable nginx >>/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
 
 echo -e "\e[33m restarting nginx server \e[0m"
-systemctl restart nginx >>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,9 @@
 echo -e "\e[31m copying mongodb repo file \e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo >>/tmp/roboshop.log
+cp /home/centos/reboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[31m installing mongodb \e[0m"
-yum install mongodb-org -y >>/tmp/roboshop.log
-#modify the config file for listner address
-echo -e "\e[31m enabling mongodb \e[0m"
-systemctl enable mongod >>/tmp/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
+echo -e "\e[31m updating mongodb listner address using sed editor \e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
 echo -e "\e[31m restarting mongodb \e[0m"
-systemctl restart mongod >>/tmp/roboshop.log
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
