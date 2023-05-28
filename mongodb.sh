3bdd4826fa12e34946b97a04d67db50f29bbc6dc
ram <ramthatigutla@gmail.com>
2023-05-28 20:17:50 +0530

    roboshop frontend check

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[31m copying mongodb repo file \e[0m"
-cp /home/centos/reboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[31m installing mongodb \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 echo -e "\e[31m updating mongodb listner address using sed editor \e[0m"
