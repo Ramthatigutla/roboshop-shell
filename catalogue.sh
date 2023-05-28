d5627c8349a35fc7632c8ac907f39ab0e5c34bc8
ram <ramthatigutla@gmail.com>
2023-05-28 20:38:06 +0530

    roboshop frontend check

--- catalogue.sh
+++ catalogue.sh
@@ -19,11 +19,11 @@ cd /app
 echo -e "\e[33m installing dependencies \e[0m"
 npm install &>>/tmp/roboshop.log
 echo -e "\e[34m setup systemD service \e[0m"
-cp /home/centos/reboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /home/centos/reboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
 echo -e "\e[31m starting catalogue server \e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl retart catalogue &>>/tmp/roboshop.log
+systemctl restart catalogue &>>/tmp/roboshop.log
 echo -e "\e[32m copying mongodb repo file \e[0m"
 cp /home/centos/reboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33m installing mongodb \e[0m"
