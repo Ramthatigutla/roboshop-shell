e9d4cf2d02ab79898dbc7141f732b187f10df894
ram <ramthatigutla@gmail.com>
2023-05-29 12:58:22 +0530

    roboshop frontend check

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,40 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
+echo -e "\e[31m configuring node js \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[32m installing nodejs\e[0m"
 yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m adding user \e[0m"
 useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[34m application directory\e[0m"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
+
+echo -e "\e[35m downloading application code \e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
+
+echo -e "\e[31m unzip application code \e[0m"
 unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 cd /app
+
+echo -e "\e[33m installing dependencies \e[0m"
 npm install &>>/tmp/roboshop.log
+
+echo -e "\e[34m setup systemD service \e[0m"
 cp /home/centos/reboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+
+echo -e "\e[31m starting catalogue server \e[0m"
 systemctl daemon-reload  &>>/tmp/roboshop.log
 systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl restart catalogue &>>/tmp/roboshop.log
-cp /home/centos/reboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
-yum install mongodb-org-shell -y  &>>/tmp/roboshop.log
+
+echo -e "\e[32m copying mongodb repo file \e[0m"
+cp /home/centos/reboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33m installing mongodb \e[0m"
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+
+echo -e "\e[34m loading schema \e[0m"
 mongo --host mongodb-dev.trrdops.store </app/schema/catalogue.js  &>>/tmp/roboshop.log
