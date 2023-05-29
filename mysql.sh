45baa8e7e5298c4386714109d3b5ba4a474da6f9
ram <ramthatigutla@gmail.com>
2023-05-29 21:56:33 +0530

    mysql component commiting

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,9 @@
 echo -e "\e[31m disable mysql default version \e[0m"
 yum module disable mysql -y &>>/tmp/roboshop.log
 
+echo -e "\e[31m copying mysql repo file \e[0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+
 echo -e "\e[31m install mysql community server \e[0m"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
 
