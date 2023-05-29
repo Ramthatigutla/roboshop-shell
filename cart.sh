35f511a12db921801de9a52862f3c296da560f91
ram <ramthatigutla@gmail.com>
2023-05-29 21:21:50 +0530

    user component commiting

---
+++ cart.sh
@@ -0,0 +1,33 @@
+echo -e "\e[31m configuring node js \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[32m installing nodejs\e[0m"
+yum install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[33m adding user \e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[34m application directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
+
+echo -e "\e[35m downloading application code \e[0m"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>>/tmp/roboshop.log
+cd /app
+
+echo -e "\e[31m unzip application code \e[0m"
+unzip /tmp/cart.zip
+cd /app
+
+echo -e "\e[33m installing dependencies \e[0m"
+npm install &>>/tmp/roboshop.log
+
+echo -e "\e[34m setup systemD service \e[0m"
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+
+echo -e "\e[31m starting cart server \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable cart &>>/tmp/roboshop.log
+systemctl restart cart &>>/tmp/roboshop.log
+
+
