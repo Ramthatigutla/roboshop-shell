a2053eb22193379799b4f77d62a1d9d64a2c561c
ram <ramthatigutla@gmail.com>
2023-05-29 18:20:30 +0530

    roboshop frontend check

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,40 @@
 echo -e "\e[31m configuring node js \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
 echo -e "\e[32m installing nodejs\e[0m"
-yum install nodejs -y &>>/tmp/roboshop.log
+yum install nodejs -y
+
 echo -e "\e[33m adding user \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+useradd roboshop
+
 echo -e "\e[34m application directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
+rm -rf /app
 mkdir /app
+
 echo -e "\e[35m downloading application code \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
 echo -e "\e[31m unzip application code \e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip
 cd /app
+
 echo -e "\e[33m installing dependencies \e[0m"
-npm install &>>/tmp/roboshop.log
+npm install
+
 echo -e "\e[34m setup systemD service \e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+
 echo -e "\e[31m starting catalogue server \e[0m"
-systemctl daemon-reload  &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+
 echo -e "\e[32m copying mongodb repo file \e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+
 echo -e "\e[33m installing mongodb \e[0m"
-yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y
+
 echo -e "\e[34m loading schema \e[0m"
-mongo --host mongodb-dev.trrdops.store </app/schema/catalogue.js  &>>/tmp/roboshop.log
+mongo --host mongodb-dev.trrdops.store </app/schema/catalogue.js
