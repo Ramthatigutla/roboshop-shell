f0c3917ca8d5a7f3e0f110ed0bc81494bc3acafd
ram <ramthatigutla@gmail.com>
2023-06-08 12:12:31 +0530

    Final shell frontend Commit

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 echo -e "\e[33m downlaoding new nginx content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[34m creating a directory to unzip downloaded content \e[0m"
+echo -e "\e[34m creating a directory to unzip the downloaded content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
