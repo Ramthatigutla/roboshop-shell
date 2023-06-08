e786a7d1a56bd9af135901b102e7918139426eef
ram <ramthatigutla@gmail.com>
2023-06-08 12:14:50 +0530

    Final shell Redis Commit

--- redis.sh
+++ redis.sh
@@ -10,6 +10,6 @@ yum install redis -y  &>>/tmp/roboshop.log
 echo -e "\e[31m updating listner address \e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf  &>>/tmp/roboshop.log
 
-echo -e "\e[31m restarting redis \e[0m"
+echo -e "\e[31m restarting redis now \e[0m"
 systemctl enable redis  &>>/tmp/roboshop.log
 systemctl restart redis  &>>/tmp/roboshop.log
