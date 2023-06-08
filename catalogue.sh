06c746b0ffdaf5054ffde9f5b4eaef22acc5b0b4
ram <ramthatigutla@gmail.com>
2023-06-08 12:44:13 +0530

    variable test shell catalogue Commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,41 +1,46 @@
-echo -e "\e[31m configuring node js \e[0m"
+component=catalogue
+color="\e[31m"
+nocolor="\e[0m"
+
+
+echo -e "$color configuring node js $nocolor"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
-echo -e "\e[32m installing nodejs\e[0m"
+echo -e "$color installing nodejs$nocolor"
 yum install nodejs -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m adding user \e[0m"
+echo -e "$color adding user $nocolor"
 useradd roboshop &>>/tmp/roboshop.log
 
-echo -e "\e[34m application directory\e[0m"
+echo -e "$color application directory$nocolor"
 rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
-echo -e "\e[35m downloading application code \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+echo -e "$color downloading application code $nocolor"
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[31m unzip application code \e[0m"
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+echo -e "$color unzip application code $nocolor"
+unzip /tmp/$component.zip &>>/tmp/roboshop.log
 cd /app
 
-echo -e "\e[33m installing dependencies \e[0m"
+echo -e "$color installing dependencies $nocolor"
 npm install &>>/tmp/roboshop.log
 
-echo -e "\e[34m setup systemD service \e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+echo -e "$color setup systemD service $nocolor"
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service  &>>/tmp/roboshop.log
 
-echo -e "\e[31m starting catalogue server \e[0m"
+echo -e "$color starting $component server $nocolor"
 systemctl daemon-reload  &>>/tmp/roboshop.log
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+systemctl enable $component &>>/tmp/roboshop.log
+systemctl restart $component &>>/tmp/roboshop.log
 
-echo -e "\e[32m copying mongodb repo file \e[0m"
+echo -e "$color copying mongodb repo file $nocolor"
 cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-echo -e "\e[33m installing mongodb \e[0m"
+echo -e "$color installing mongodb $nocolor"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
-echo -e "\e[34m loading schema now \e[0m"
-mongo --host mongodb-dev.trrdops.store </app/schema/catalogue.js  &>>/tmp/roboshop.log
+echo -e "$color loading schema now $nocolor"
+mongo --host mongodb-dev.trrdops.store </app/schema/$component.js  &>>/tmp/roboshop.log
 
